f1c851afdb149d43ebe20450489a6cb01f66c79e
Hiichan75 <kimsojung19950712@gmail.com>
2025-11-26 10:02:45 +0100

    startup

---
+++ src/main/java/com/example/lolstats/LolStatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lolstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolStatsApplication.class, args);
+	}
+
+}
